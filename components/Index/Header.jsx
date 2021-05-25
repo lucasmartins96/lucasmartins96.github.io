ac54ea972a866e01b34a6c9fa601d1916fe9881f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-24 21:03:29 -0300

    Configurações iniciais do Header

---
+++ components/Index/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <div>
+          <a href="#">Lucas</a>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a>home</a>
+            </li>
+            <li>
+              <a>sobre mim</a>
+            </li>
+            <li>
+              <a>projetos desenvolvidos</a>
+            </li>
+            <li>
+              <a>contato</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
