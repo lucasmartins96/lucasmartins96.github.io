dba8955c10e1d5ab6af0747b4469e3e7a376f981
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-22 11:37:10 -0300

    adicionando o componente Wrapper

---
+++ layout/Wrapper.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ComponentWrapper = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection || 'row'};
+  justify-content: ${(props) => props.justifyContent || 'flex-start'};
+  margin: 0 auto;
+  max-width: 900px;
+`;
+
+function Wrapper({ children, flexDirection, justifyContent }) {
+  return (
+    <ComponentWrapper flexDirection={flexDirection} justifyContent={justifyContent}>
+      {children}
+    </ComponentWrapper>
+  );
+}
+
+export default Wrapper;
