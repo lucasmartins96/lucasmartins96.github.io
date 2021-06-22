565d191299352b0e29909be12ba2643e543c88ac
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-22 11:44:03 -0300

    retorando o estilo global da aplicação

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
   }
 `;
 
