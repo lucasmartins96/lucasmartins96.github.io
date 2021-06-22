8de295b1af89f82f2430815ca8d92763af721955
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-22 12:24:36 -0300

    adicionando fonte

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    font-family: 'Montserrat', sans-serif;
     font-size: 1.6rem;
   }
 `;
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,16 @@ function HomePage() {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
         <title>Lucas Martins da Silva</title>
       </Head>
       <Header />
