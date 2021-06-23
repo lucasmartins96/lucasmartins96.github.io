8d2072489907fd2244abfae8c028a09c42d3850d
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-23 00:05:37 -0300

    importando CircleFill e adicionando o conteúdo textual do Hero

--- components/Index/Hero.jsx
+++ components/Index/Hero.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { CircleFill } from 'react-bootstrap-icons';
 
 function Hero() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>
+        Olá... Eu sou Lucas
+        Desenvolvedor Web
+      </h1>
+      <ul>
+        <li>Estudante</li>
+        <CircleFill size={14}/>
+        <li>Desenvolvedor</li>
+        <CircleFill size={14}/>
+        <li>Músico</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Hero;
