663941291ac0002cb86a03c0cf70af561c738939
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-22 23:06:41 -0300

    corrigindo a propriedade do DOM

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ function HomePage() {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
           rel="stylesheet"
