43c46475080061a8bb3555822d7f1cf91d1f5c8c
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-17 17:08:27 -0300

    implementando o componente Hero

--- components/Index/Hero.jsx
+++ components/Index/Hero.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import { CircleFill } from 'react-bootstrap-icons';
+import styled from 'styled-components';
+import wallpaper from '../../public/2109964.jpg';
+
+const HeroImage= styled.div`
+  align-items: center;
+  background-image: url(${wallpaper});
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+`;
+
+const HeroMessage = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  height: 30%;
+  justify-content: space-evenly;
+  width: 900px;
+  text-align: center;
+`;
 
 function Hero() {
   return (
-    <div>
-      <h1>
-        Olá... Eu sou Lucas
-        Desenvolvedor Web
-      </h1>
-      <ul>
-        <li>Estudante</li>
-        <CircleFill size={14}/>
-        <li>Desenvolvedor</li>
-        <CircleFill size={14}/>
-        <li>Músico</li>
-      </ul>
-    </div>
+    <HeroImage>
+      <HeroMessage>
+        <h1>Lucas Martins da Silva</h1>
+        <span>
+          Estudante <CircleFill size={7} /> Desenvolvedor <CircleFill size={7} /> Músico{' '}
+        </span>
+      </HeroMessage>
+    </HeroImage>
   );
 }
 
