686bf03213d9cd2e864fc2c88947b7b341621df0
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-17 19:59:56 -0300

    adicionando wrapper da imagem

--- components/Index/About.jsx
+++ components/Index/About.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import Wrapper from '../../layout/Wrapper';
 import Image from 'next/image';
 import profileImage from '../../public/lucas_martins_2.jpg';
+import styled from 'styled-components';
+
+const WrapperImage = styled.div`
+  border: 1px solid black;
+  width: 40%;
+`;
 
 function About() {
-  const widHeiImage = 300;
+  const widHeiImage = 1440;
   return (
     <Wrapper>
-      <Image
-        src={profileImage}
-        alt="minha foto"
-        width={widHeiImage}
-        height={widHeiImage}
-      />
-
+      <WrapperImage>
+        <Image
+          src={profileImage}
+          alt="minha foto"
+          width={widHeiImage}
+          height={widHeiImage}
+        />
+      </WrapperImage>
     </Wrapper>
   )
 }
