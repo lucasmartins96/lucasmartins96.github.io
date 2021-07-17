885a2a21784fca03a19f1de9a8c3ff38f1a3efeb
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-17 19:01:20 -0300

    adicionando a imagem

--- components/Index/About.jsx
+++ components/Index/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Wrapper from '../../layout/Wrapper';
+import Image from 'next/image';
+import profileImage from '../../public/lucas_martins_2.jpg';
 
 function About() {
+  const widHeiImage = 300;
   return (
     <Wrapper>
-      
+      <Image
+        src={profileImage}
+        alt="minha foto"
+        width={widHeiImage}
+        height={widHeiImage}
+      />
 
     </Wrapper>
   )
