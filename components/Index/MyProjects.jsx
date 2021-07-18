f1178616f506ecf7a51c9cb79908cb55c230b75f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-17 21:51:03 -0300

    implementando o esqueleto do componente MyProjects

--- components/Index/MyProjects.jsx
+++ components/Index/MyProjects.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import styled from 'styled-components';
 import Wrapper from '../../layout/Wrapper';
+
+const MyProjectsList = styled.section`
+  display: grid;
+  gap: 4% 1.25%;
+  grid-template-columns: repeat(3, 1fr);
+  height: 50vh;
+  width: 100%;
+`;
+
+const MyProjectCard = styled.div`
+  background-color: #295fa5;
+  display: flex;
+
+  div {
+    align-self: flex-end;
+    background-color: #e1e7ef;
+    padding: 10px 20px;
+    width: 100%;
+  }
+`;
 
 function MyProjects() {
   return (
-    <Wrapper>
+    <Wrapper flexDirection="column">
       <h3>Projetos desenvolvidos</h3>
-
+      <MyProjectsList>
+        <MyProjectCard><div>1</div></MyProjectCard>
+        <MyProjectCard><div>2</div></MyProjectCard>
+        <MyProjectCard><div>3</div></MyProjectCard>
+        <MyProjectCard><div>4</div></MyProjectCard>
+        <MyProjectCard><div>5</div></MyProjectCard>
+        <MyProjectCard><div>6</div></MyProjectCard>      
+      </MyProjectsList>
     </Wrapper>
   )
 }
